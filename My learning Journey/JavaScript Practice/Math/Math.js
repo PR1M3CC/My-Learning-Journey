f3625eaae360768fa5ad43d5210ae2b909180363
Usman Khan <primekk3@gmail.com>
2025-06-16 17:14:21 +0500

    still learning JS lol

---
+++ My learning Journey/JavaScript Practice/Math/Math.js
@@ -0,0 +1,5 @@
+console.log(Math.floor(Math.random()*10) + 1);
+// If we want to define a custom range, we can use this formula:
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
