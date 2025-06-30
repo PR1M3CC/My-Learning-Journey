e457b285c813dde1d4b6ffd1253f1341612d7b96
Usman Khan <primekk3@gmail.com>
2025-06-30 20:38:09 +0500

    learnt about react props

---
+++ My learning Journey/React Learning/props/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from './Components/card'
+
+function App() {
+
+
+  return (
+    <>
+    <Card buttonName= "Chai" />
+    <Card buttonName='Usman'/>
+    </>
+  )
+}
+
+export default App
