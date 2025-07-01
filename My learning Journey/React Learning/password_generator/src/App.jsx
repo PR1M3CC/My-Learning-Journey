0d4209aefc175b1da531a2aa12860a293aea26a9
Usman Khan <primekk3@gmail.com>
2025-07-01 18:26:10 +0500

    UI changes

--- My learning Journey/React Learning/password_generator/src/App.jsx
+++ My learning Journey/React Learning/password_generator/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
 
     // Save generated password to state
     setPassword(pass);
-  }, [length, numAllowed, charAllowed, setPassword]);
+  }, [length, numAllowed, charAllowed,]);
 
   // Function to copy password to clipboard
   const copyPasswordToClipboard = useCallback(() => {
@@ -84,7 +84,7 @@ function App() {
                 value={length}
                 className="cursor-pointer"
                 onChange={(e) => {
-                  setLength(e.target.value); // update password length
+                  setLength(Number(e.target.value)); // update password length
                 }}
               />
               <label>Length: {length}</label>
@@ -94,7 +94,7 @@ function App() {
             <div className="flex gap-x-1 items-center">
               <input
                 type="checkbox"
-                defaultChecked={numAllowed}
+                checked={numAllowed}
                 id="numberInput"
                 onChange={() => {
                   setNumAllowed((prev) => !prev); // toggle number inclusion
@@ -107,7 +107,7 @@ function App() {
             <div className="flex items-center gap-x-1">
               <input
                 type="checkbox"
-                defaultChecked={charAllowed}
+                checked={charAllowed}
                 id="characterInput"
                 onChange={() => {
                   setCharAllowed((prev) => !prev); // toggle character inclusion
